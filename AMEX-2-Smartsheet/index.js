6323049a2bea8cbafe2952a6024f1516094fbfc1
bpware@smart-egg <tech@smart-egg.com>
2019-04-27 01:37:01 +0200

    added filename test

--- AMEX-2-Smartsheet/index.js
+++ AMEX-2-Smartsheet/index.js
@@ -1,10 +1,10 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.query.name || (req.body && req.body.filename)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello " + (req.query.name || req.body.filename)
         };
     }
     else {
