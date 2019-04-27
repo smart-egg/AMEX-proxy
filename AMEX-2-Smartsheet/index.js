97d237b46062db482480d3fd9de33f6b089b94ae
Hector19901129 <41280117+Hector19901129@users.noreply.github.com>
2019-04-27 15:57:12 +0800

    test

--- AMEX-2-Smartsheet/index.js
+++ AMEX-2-Smartsheet/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.filename)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.filename)
+            body: "Hello " + (req.query.name || req.body.name)
         };
     }
     else {
