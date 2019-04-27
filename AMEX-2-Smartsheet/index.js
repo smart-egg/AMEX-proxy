8fa7da28200b850e9220c3155e44ec158d94b335
Hector19901129 <41280117+Hector19901129@users.noreply.github.com>
2019-04-27 16:26:43 +0800

    test1

--- AMEX-2-Smartsheet/index.js
+++ AMEX-2-Smartsheet/index.js
@@ -1,16 +1,28 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.filename)) {
+    if (req.body && req.body.filename && req.body.contents && req.body.smartsheet_id && req.body.account_name) {
+        var filename = req.body.filename;
+        var b = new Buffer(req.body.contents, 'base64')
+        var contents = b.toString();
+        var smartsheet_id = req.body.smartsheet_id;
+        var account_name = req.body.account_name;
+        var body = {
+            "status": 200,
+            "message": "success",
+            "filename": filename,
+            "smartsheet_id": smartsheet_id,
+            "account_name": account_name,
+        }
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: body
         };
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a correct parameters in the request body"
         };
     }
 };
